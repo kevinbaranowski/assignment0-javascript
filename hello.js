d2008a181def400df30b9c6e60058b94ba06c63c
kevin baranowski <kevindbaranowski@gmail.com>
2024-03-25 12:12:06 -0500

    testing

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
-	return 'Hello world!';
+	return 'Hello world! My name is Kevin.';
 }
 
 module.exports = hello;
